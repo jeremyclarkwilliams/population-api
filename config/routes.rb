8803e7612d381f1730eaa611b0a4ccc9b4ca395d
Jeremy Williams <jeremy@jeremyclarkwilliams.com>
2018-07-28 14:18:44 -0700

    Update routes with api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+PopulationApi::Application.routes.draw do
+
+    # Api definition
+    namespace :api, defaults: { format: :json },
+                    path: '/api/'  do
+      # We are going to list our resources here
+    end
+
 end
